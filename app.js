c85564975b1df0bb14d8d757737c4b2702c8b514
Shey Njila <sheynjila.devtest@gmail.com>
2022-12-07 10:57:40 -0500

    changes to app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! This is the change made by Shey Njila to test Webhook in the pipeline Manual Approval'));
+app.get('/', (req, res) => res.send('Hello World! This is the change made by Shey Njila to test githb'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
